06707f577bb9013ab0f919c89df882b40bd6486f
anisulca <nanisulca@gmail.com>
2020-11-06 19:27:48 -0300

    Este es el proyecto de unity

---
+++ teste2/Assets/Scripts/BotonRojoCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonRojoCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 30; //La variable velocidad es publica para que no 
+    //sea necesario modificarla en el Script
+    // Use this for initialization
+    void Start () {
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+
+    }
+
+    // Update is called once per frame
+    //void Update () {
+   
+        
+    
+}
